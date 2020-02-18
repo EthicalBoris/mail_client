dc5570934041314c609eb5074365eb51ff5fb01d
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2020-02-18 23:55:44 +0000

    Automatic serialization
    - Implemented automatic serialization of email previews from Json

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.cyan,
         //accentColor: Colors.green,
       ),
       home: MessageList(title: 'Bobo Mail Service'),
