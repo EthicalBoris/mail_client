1e8ed5a404909cd7ddb6b18cb8f3ac96b4139b0f
EthicalBoar <38215510+EthicalBoris@users.noreply.github.com>
2020-02-26 03:58:16 +0000

    Better forms
    + The form in the message compose page now uses textFormField widgets instead of TextFields

--- lib/MessageCompose.dart
+++ lib/MessageCompose.dart
@@ -14,6 +14,8 @@ class _MessageComposeState extends State<MessageCompose> {
   String subject = "";
   String body = "";
 
+  final key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,56 +23,54 @@ class _MessageComposeState extends State<MessageCompose> {
         title: Text("Compose New Message"),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            ListTile(
-              title: TextField(
-                onChanged: (value) {
-                  to = value;
-                },
-                decoration: InputDecoration(
-                  labelText: "To",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+        child: Form(
+          key: key,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ListTile(
+                title: TextFormField(
+                  onSaved: (value) => to = value,
+                  decoration: InputDecoration(
+                    labelText: "To",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-            ListTile(
-              title: TextField(
-                onChanged: (value) {
-                  subject = value;
-                },
-                decoration: InputDecoration(
-                  labelText: "Subject",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              Divider(),
+              ListTile(
+                title: TextFormField(
+                  onSaved: (value) => subject = value,
+                  decoration: InputDecoration(
+                    labelText: "Subject",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-            ListTile(
-              title: TextField(
-                onChanged: (value) {
-                  body = value;
-                },
-                decoration: InputDecoration(
-                  labelText: "Message",
-                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              Divider(),
+              ListTile(
+                title: TextFormField(
+                  onSaved: (value) => body = value,
+                  decoration: InputDecoration(
+                    labelText: "Message",
+                    labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  maxLines: 8,
                 ),
-                maxLines: 8,
               ),
-            ),
-            ListTile(
-              title: RaisedButton(
-                child: Text("Send"),
-                onPressed: () {
-                  Message message = Message(subject, body);
+              ListTile(
+                title: RaisedButton(
+                  child: Text("Send"),
+                  onPressed: () {
+                    this.key.currentState.save();
 
-                  Navigator.pop(context, message);
-                },
+                    Message message = Message(subject, body);
+                    Navigator.pop(context, message);
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
